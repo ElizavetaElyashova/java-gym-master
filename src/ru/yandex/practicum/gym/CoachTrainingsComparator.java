2730694d3897f2b232dac18ecf701886c7ec0277
eelyashova <elyashova.liza@yandex.ru>
2026-03-30 11:34:01 +0300

    remove redundant comments; add null checks in Timetable; change CoachTrainingsComparator

--- src/ru/yandex/practicum/gym/CoachTrainingsComparator.java
+++ src/ru/yandex/practicum/gym/CoachTrainingsComparator.java
@@ -5,7 +5,11 @@ import java.util.Comparator;
 public class CoachTrainingsComparator implements Comparator<Coach> {
     @Override
     public int compare(Coach c1, Coach c2) {
-        return Integer.compare(c1.getNumberOfTrainingSessions(), c2.getNumberOfTrainingSessions());
+        if (c1.getNumberOfTrainingSessions().equals(c2.getNumberOfTrainingSessions())) {
+            return String.CASE_INSENSITIVE_ORDER.reversed().compare(c1.getSurname(), c2.getSurname());
+        } else {
+            return Integer.compare(c1.getNumberOfTrainingSessions(), c2.getNumberOfTrainingSessions());
+        }
     }
 }
 
