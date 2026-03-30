1b35c7c641cfc00f80d574aa4d90fe32ea3febfc
eelyashova <elyashova.liza@yandex.ru>
2026-03-30 21:02:45 +0300

    change arguments in return String... statement in CoachTrainingsComparator

--- src/ru/yandex/practicum/gym/CoachTrainingsComparator.java
+++ src/ru/yandex/practicum/gym/CoachTrainingsComparator.java
@@ -6,7 +6,7 @@ public class CoachTrainingsComparator implements Comparator<Coach> {
     @Override
     public int compare(Coach c1, Coach c2) {
         if (c1.getNumberOfTrainingSessions().equals(c2.getNumberOfTrainingSessions())) {
-            return String.CASE_INSENSITIVE_ORDER.reversed().compare(c1.getSurname(), c2.getSurname());
+            return String.CASE_INSENSITIVE_ORDER.reversed().compare(c1.toString(), c2.toString());
         } else {
             return Integer.compare(c1.getNumberOfTrainingSessions(), c2.getNumberOfTrainingSessions());
         }
